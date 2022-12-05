580e9c49cda312f9be0d705a03dc57f40222a69d
hy0708@cau.ac.kr <hy0708@cau.ac.kr>
2022-12-05 20:10:14 +0900

    1201

---
+++ calcu.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS    // scanf 보안 경고로 인한 컴파일 에러 방지
+#include <stdio.h>
+/*
+ * int main(){
+    int a,b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    return 0;
+}*/
+/*
+ * int main(){
+    char a[50];
+    scanf("%s", &a);
+    printf("%s??!\n", a);
+    return 0;
+}*/
+/*
+ * int main(){
+    int a;
+    scanf("%d", &a);
+    printf("%d", a-543);
+    return 0;
+}*/
