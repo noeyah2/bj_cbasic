cfd8b38ef30a5e515572fb47e14ca2ca391270b6
hy0708@cau.ac.kr <hy0708@cau.ac.kr>
2022-12-05 20:12:55 +0900

    1201

---
+++ 3003.c
@@ -0,0 +1,15 @@
+//
+// Created by 하연 on 2022/12/05.
+//
+#include <stdio.h>
+
+int main(){
+    int king = 1;
+    int queen = 1;
+    int look = 2;
+    int bshop = 2;
+    int knight = 2;
+    int phone = 8;
+
+
+}
