d6f13b52b9d482674189589b72cfe6ee6c70b793
hy0708@cau.ac.kr <hy0708@cau.ac.kr>
2022-12-05 20:35:08 +0900

    1205

---
+++ 10430.c
@@ -0,0 +1,11 @@
+//
+// Created by 하연 on 2022/12/05.
+//
+/*#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%d\n%d\n%d\n", (a+b)%c, ((a%c)+(b%c))%c, (a*b)%c, ((a%c)*(b%c))%c);
+    return 0;
+}*/
--- 3003.c
+++ 3003.c
@@ -1,15 +1,16 @@
 //
 // Created by 하연 on 2022/12/05.
 //
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main(){
-    int king = 1;
-    int queen = 1;
-    int look = 2;
-    int bshop = 2;
-    int knight = 2;
-    int phone = 8;
-
-
-}
+    int king;
+    int queen;
+    int rook;
+    int bishop;
+    int knight;
+    int pawn;
+    scanf("%d %d %d %d %d %d", &king, &queen, &rook, &bishop, &knight, &pawn);
+    printf("%d %d %d %d %d %d", 1-king,1-queen,2-rook,2-bishop,2-knight,8-pawn);
+    return 0;
+}*/
